27a4045693b448addbe45b9f9360a41a11469520
Rimjhim Agrawal <101130751+rimjhim-ag@users.noreply.github.com>
2025-08-18 10:09:47 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://whatsapp-backend-rimjhim.vercel.app/api/messages"; // your backend base URL
+const BASE_URL = PROCESS.ENV.BACKEND_URL || " "; // your backend base URL
 
 // Fetch the list of chats (contacts with last message info)
 export async function fetchChats() {
